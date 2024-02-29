899263472435fc6f8c1424375cc68d05766976ce
Kürşat Çağrı Yakıcı <kcyakici@hotmail.com>
2024-02-29 16:53:42 +0300

    fix jwt secret

--- src/main/java/com/example/jwtdemo/utils/JwtUtil.java
+++ src/main/java/com/example/jwtdemo/utils/JwtUtil.java
@@ -17,7 +17,7 @@ import io.jsonwebtoken.security.Keys;
 public class JwtUtil {
 
 
-    public static final String SECRET = "5367566B59703373367639792F423F45"; // 32 characters for 256 bits
+    public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437"; // 32 characters for 256 bits
     public static final int tokenDuration = 1000*60*60;
 
     public String extractUsername(String token) {
