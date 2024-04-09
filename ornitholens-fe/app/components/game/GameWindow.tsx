fd8e5196b0cd3181cd45514cc306c911f9747baf
Kürşat Çağrı Yakıcı <kcyakici@hotmail.com>
2024-04-09 15:59:22 +0300

    refactor game components and parse correct name of the bird in the image

--- ornitholens-fe/app/components/game/GameWindow.tsx
+++ ornitholens-fe/app/components/game/GameWindow.tsx
@@ -6,6 +6,8 @@ import React from "react";
 
 export default async function GameWindow() {
   let imageSrc = "";
+  let correctAnswer = "";
+  let possibleAnswersArray: string[] = [];
   try {
     const dir = "public\\images";
     const randomFolder = await chooseRandomFolder(dir);
@@ -13,13 +15,26 @@ export default async function GameWindow() {
     const randomFile = await chooseRandomFolder(randomFolder);
     console.log(`Bird: ${randomFile}`);
     imageSrc = "/" + randomFile.replaceAll("\\", "/").substring(7); // get rid of /public
+    correctAnswer = parseBirdName(imageSrc);
+    console.log(`Correct answer: ${correctAnswer}`);
   } catch (err) {
     console.error("Error occurred:", err);
   }
   return (
-    <Box>
+    <div>
       <BirdPicture imageSrc={imageSrc} />
-      {/* <AnswerButtonWrapper></AnswerButtonWrapper> */}
-    </Box>
+      <AnswerButtonWrapper
+        correctAnswer="Penguin"
+        answersForButtons={["Penguin", "Albatros", "Sparrow", "Pigeon"]}
+      ></AnswerButtonWrapper>
+    </div>
   );
+}
+
+function parseBirdName(imageSrc: string) {
+  return imageSrc
+    .substring(imageSrc.lastIndexOf("/") + 1, imageSrc.lastIndexOf("."))
+    .split("_")
+    .slice(0, -2)
+    .join(" ");
 }
--- ornitholens-fe/app/play/page.tsx
+++ ornitholens-fe/app/play/page.tsx
@@ -5,36 +5,14 @@ import {
 } from "../utils/FileOperations";
 import { Box } from "@mui/material";
 import AnswerButtonWrapper from "../components/game/AnswerButtonWrapper";
+import BirdPicture from "../components/game/BirdPicture";
+import GameWindow from "../components/game/GameWindow";
 
 export default async function PlayPage() {
-  let imageSrc = "";
-  try {
-    const dir = "public\\images";
-    const randomFolder = await chooseRandomFolder(dir);
-    console.log(`Bird: ${randomFolder}`);
-    const randomFile = await chooseRandomFolder(randomFolder);
-    console.log(`Bird: ${randomFile}`);
-    imageSrc = "/" + randomFile.replaceAll("\\", "/").substring(7); // get rid of /public
-  } catch (err) {
-    console.error("Error occurred:", err);
-  }
-
   return (
-    <Box>
+    <div>
       <h1>Play page</h1>
-      <Image
-        src={imageSrc}
-        alt="An image of a bird"
-        priority
-        width={0}
-        height={0}
-        sizes="100vw"
-        style={{ width: "20%", height: "auto" }}
-      ></Image>
-      <AnswerButtonWrapper
-        correctAnswer="Penguin"
-        answersForButtons={["Penguin", "Albatross", "Sparrow", "Pigeon"]}
-      ></AnswerButtonWrapper>
-    </Box>
+      <GameWindow />
+    </div>
   );
 }
