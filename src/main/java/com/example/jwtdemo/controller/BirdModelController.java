5291f7f539c1c23681dd7f157f8d34504d2aa892
Kürşat Çağrı Yakıcı <kcyakici@hotmail.com>
2024-04-10 16:34:49 +0300

    save the uploaded image

--- src/main/java/com/example/jwtdemo/controller/BirdModelController.java
+++ src/main/java/com/example/jwtdemo/controller/BirdModelController.java
@@ -1,6 +1,8 @@
 package com.example.jwtdemo.controller;
 
 import com.example.jwtdemo.dto.IdentificationRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,8 +11,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+
 @RestController
 public class BirdModelController {
+
+    private String UPLOAD_PATH; // TODO get relative path
+
     @PostMapping("/upload")
     @CrossOrigin
     public ResponseEntity<String> uploadFile(IdentificationRequest identificationRequest) {
@@ -20,6 +27,12 @@ public class BirdModelController {
         // System.out.println("To string method result: " + multipartFile.toString());
         System.out.println("Name of the imageFile: " + multipartFile.getOriginalFilename());
 
-        return new ResponseEntity<>("File is received successfully", HttpStatus.OK);
+        try {
+            multipartFile.transferTo(new File(UPLOAD_PATH + multipartFile.getOriginalFilename()));
+            return new ResponseEntity<>("File uploaded successfully.", HttpStatus.OK);
+        } catch (Exception e) {
+            System.err.println("Exception occurred: " + e.getMessage());
+            return new ResponseEntity<>("File uploading has failed", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
